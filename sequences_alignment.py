93cc366def54e545375f42cc7a97fe9aaed07713
Babar <bapt09.rousseau@gmail.com>
2021-11-09 13:00:13 +0100

    minor fixes

--- sequences_alignment.py
+++ sequences_alignment.py
@@ -1,34 +1,47 @@
+"""This module align a multi sequences fasta file.
+
+ClustalW and Muscle algorithms are both supported for the alignment.
+"""
+
 from io import StringIO
 from Bio import AlignIO
-from Bio.Align.Applications import ClustalOmegaCommandline
+from Bio.Align.Applications import ClustalOmegaCommandline, MuscleCommandline
 
 
 def align_seq(in_dir, method="clustalw", out_file=False):
     """Align multiple sequences using the MUSCLE algorithm.
-    
+
     Parameters
     ----------
     in_dir : dir
         The directory to the file containing the sequences.
-    out_dir : boolean
+    method : str (default clustalw)
+        The alignment algorithm, either clustalw or muscle.
+    out_dir : boolean (default False)
         Wether or not a output file should be generated.
-    
+
     Returns
     -------
     align
         The aligned sequences.
     """
-    clustalo_exe = ".\clustal-omega\clustalo.exe"
-    # muscle_exe = ".\muscle3.8.31_i86win32.exe"
-    
+    clustalo_exe = "./clustal-omega/clustalo.exe"
+    muscle_exe = "./muscle3.8.31_i86win32.exe"
+
     if out_file:
         out_dir = in_dir.replace(".fasta", "_aligned.fasta")
         open(out_dir, "w+").close()
-        clustalw_cline = ClustalOmegaCommandline(clustalo_exe,
-                                                 infile=in_dir,
-                                                 outfile=out_dir,
-                                                 force=True)
-        stdout, stderr = clustalw_cline()
+        if method == "clustalw":
+            cline = ClustalOmegaCommandline(clustalo_exe,
+                                            infile=in_dir,
+                                            outfile=out_dir,
+                                            force=True)
+        if method == "muscle":
+            cline = MuscleCommandline(muscle_exe,
+                                      infile=in_dir,
+                                      outfile=out_dir,
+                                      force=True)
+        stdout, stderr = cline()
         align = AlignIO.read(out_dir, "fasta")
 
     else:
@@ -39,6 +52,7 @@ def align_seq(in_dir, method="clustalw", out_file=False):
         align = AlignIO.read(StringIO(stdout), "fasta")
     return align
 
+
 if __name__ == "__main__":
-    seq_file = "data/spike_data_708.fasta"
-    sequence = align_seq(seq_file, "clustalw", True)
+    SEQ_FILE = "data/spike_data_708.fasta"
+    sequence = align_seq(SEQ_FILE, "clustalw", True)
--- tree_generation.py
+++ tree_generation.py
@@ -1,39 +1,48 @@
+"""This module generate trees based on a multi sequences alignment or \
+does perform the alignment in case it has not been preiviously done.
+
+See the documentation of the sequences_alignment module for the alignment.
+
+NJ and UPGMA algorithms are both supported for the tree generation.
+"""
+
 import os
 import matplotlib.pyplot as plt
 import Bio.Phylo.TreeConstruction as Tree
 from Bio import AlignIO, Phylo
 
+# Custom alignement module, see its documentation
 from sequences_alignment import align_seq
 
 
 def draw_tree(tree, method):
     """Save a phylogenetic tree as a svg file.
-    
+
     Parameters
     ----------
     tree : Bio.Phylo.BaseTree.Tree
         The phylogenetic tree to save.
     method : str
-        The method used to generate the tree, used for the file name. 
+        The method used to generate the tree, used for the file name.
     """
     fig = plt.figure(figsize=(10, 20), dpi=100)
     axes = fig.add_subplot(1, 1, 1)
     Phylo.draw(tree, axes=axes, do_show=False)
-    plt.savefig(seq_file.replace(".fasta", "_"+method+".svg"))
+    plt.savefig(method+".svg")
 
 
-def tree_constructor(seq, method, svg):
+def tree_constructor(seq, method="nj", svg=False):
     """Construct a tree using the NJ or UPGMA method.
-    
+
     Parameters
     ----------
     seq :Bio.Align.MultipleSeqAlignment
         A multiple sequences alignement.
-    method : str
+    method : str (default nj)
         Indicates which method to use, NJ or UPGMA.
-    svg : boolean
+    svg : boolean (default False)
         Wether or not the tree should be saved as a svg file.
-    
+
     Returns
     -------
     Bio.Phylo.BaseTree.Tree
@@ -42,86 +51,90 @@ def tree_constructor(seq, method, svg):
     calculator = Tree.DistanceCalculator('blosum62')
     distance_matrix = calculator.get_distance(seq)
     constructor = Tree.DistanceTreeConstructor()
-    
-    if method == "NJ":
+
+    if method == "nj":
         tree = constructor.nj(distance_matrix)
-    if method == "UPGMA":
+    if method == "upgma":
         tree = constructor.upgma(distance_matrix)
-    
-    Phylo.write(tree, seq_file.replace(".fasta", "_"+method+".nwk"), "newick")
-    
+
+    Phylo.write(tree, method+".nwk", "newick")
+
     if svg:
         draw_tree(tree, method)
     return tree
 
 
-def parsimony_constructor(tree, seq, svg):
+def parsimony_constructor(tree, seq, svg=False):
     """Construct a tree using the parsimony method.
-    
+
     Parameters
     ----------
     tree : Bio.Phylo.BaseTree.Tree
         A model tree to use as comparison.
     seq : Bio.Align.MultipleSeqAlignment
         A multiple sequences alignement.
-    svg : boolean
-        Wether or not the tree should be saved as a svg file. 
+    svg : boolean (default False)
+        Wether or not the tree should be saved as a svg file.
     """
     scorer = Tree.ParsimonyScorer()
     searcher = Tree.NNITreeSearcher(scorer)
     constructor = Tree.ParsimonyTreeConstructor(searcher, tree)
-    
+
     pars_tree = constructor.build_tree(seq)
-    Phylo.write(pars_tree,
-                seq_file.replace(".fasta", "_parsimony.nwk"),
-                "newick")
-    
+    Phylo.write(pars_tree, "parsimony.nwk", "newick")
+
     if svg:
-            draw_tree(pars_tree, "parsimony")
+        draw_tree(pars_tree, "parsimony")
 
 
-def tree_manager(seq, aligned=False, method="nj", svg=False, parsimony=False):
-    """Construct a tree from a multi sequence alignement
+def tree_manager(seq, aligned=False, align_method="clustalw", align_out=False,
+                 tree_method="nj", svg=False, parsimony=False):
+    """Construct a tree from a multi sequence alignement \
     or from a multi sequence file that will get aligned.
-    
+
     Parameters
     ----------
     seq : Bio.Align.MultipleSeqAlignment
         A multiple sequences alignement.
-    aligned : boolean
+    aligned : boolean (default False)
         Wether or not the seq specified is already aligned.
-    method : str
+    align_method : str (default clustalw)
+        The alignment algorithm, either clustalw or muscle.
+    align_out : boolean (default False)
+        Wether or not an output file should be generated from the alignment.
+    tree_method : str (default nj)
         The method to use for the tree generation, either NJ or UPGMA.
-    svg : boolean
-        Wether or not the tree should be saved as a svg file. 
-    parsimony : boolean
-        Wether or not a parsimony tree should be generated. 
+    svg : boolean (default False)
+        Wether or not the tree should be saved as a svg file.
+    parsimony : boolean (default False)
+        Wether or not a parsimony tree should be generated.
     """
     if isinstance(seq, str):
         if os.path.isfile(seq):
             if aligned:
-                sequence = AlignIO.read(seq)
+                sequence = AlignIO.read(seq, "fasta")
             else:
-                sequence = align_seq(seq)
-        else :
+                sequence = align_seq(seq, align_method, align_out)
+        else:
             print("Error: Not a valid file.")
     elif isinstance(seq, AlignIO.MultipleSeqAlignment):
         sequence = seq
     else:
         print("Error: Input sequence need to be a fasta file \
               or an multi sequences alignment.")
-    
-    if method == "nj" or method == "upgma":   
-        tree = tree_constructor(sequence, method, svg)
+
+    if tree_method in ('nj', 'upgma'):
+        tree = tree_constructor(sequence, tree_method, svg)
     else:
         print("Error: method should either be 'nj' or 'upgma'. \
               Leave blank for nj method.")
 
     if parsimony:
-        parsimony_constructor(tree, seq, svg)
+        parsimony_constructor(tree, svg)
+
 
 if __name__ == "__main__":
-    seq_file = "data/spike_data_708.fasta"
-    aligned_seq = AlignIO.read(seq_file.replace(".fasta", "_aligned.fasta"),
+    SEQ_FILE = "data/spike_data_708.fasta"
+    aligned_seq = AlignIO.read(SEQ_FILE.replace(".fasta", "_aligned.fasta"),
                                "fasta")
-    tree_manager(aligned_seq, True, svg=True)
+    tree_manager(SEQ_FILE, aligned=True, svg=True)
