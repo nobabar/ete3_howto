184f40d8641310e33711be698455bfab1fa8ae5b
Babar <bapt09.rousseau@gmail.com>
2021-11-26 19:01:47 +0100

    cleaned modules and scripts

--- sequences_alignment.py
+++ sequences_alignment.py
@@ -3,49 +3,57 @@
 ClustalW and Muscle algorithms are both supported for the alignment.
 """
 
+
+from io import StringIO
+
 from Bio import AlignIO
 from Bio.Align.Applications import ClustalOmegaCommandline, MuscleCommandline
-from io import StringIO
 
 
 def align_seq(in_dir, method="clustalw", out_file=False):
-    """Align multiple sequences using the MUSCLE algorithm.
+    """Align multiple sequences.
 
     Parameters
     ----------
-    in_dir : dir
+    in_dir : str
         The directory to the file containing the sequences.
     method : str (default clustalw)
         The alignment algorithm, either clustalw or muscle.
     out_dir : boolean (default False)
-        Wether or not a output file should be generated.
+        Wether or not an output file should be generated.
 
     Returns
     -------
     Bio.Align.MultipleSeqAlignment
         The aligned sequences.
     """
-    clustalo_exe = "./clustal-omega/clustalo.exe"
-    muscle_exe = "./muscle3.8.31_i86win32.exe"
-
     if out_file:
+        # create the output file or make sure it is empty
         out_dir = in_dir.replace(".fasta", "_aligned.fasta")
         open(out_dir, "w+").close()
-        parameters = {"infile": in_dir, "outfile": out_dir, "force": True}
+
+        # call the correct alignement method via command line
         if method == "clustalw":
-            cline = ClustalOmegaCommandline(clustalo_exe, parameters)
+            cline =  ClustalOmegaCommandline(infile=in_dir,
+                                             outfile=out_file,
+                                             force=True)
         elif method == "muscle":
-            cline = MuscleCommandline(muscle_exe, parameters)
-        stdout, stderr = cline()
+            cline = MuscleCommandline(input=in_dir,
+                                      out=out_file)
+        _ = cline()
+
+        # read the output file
         align = AlignIO.read(out_dir, "fasta")
 
     else:
-        parameters = {"infile": in_dir, "auto": True}
+        # call the correct alignement method via command line
         if method == "clustalw":
-            cline = ClustalOmegaCommandline(clustalo_exe, parameters)
+            cline =  ClustalOmegaCommandline(infile=in_dir, auto=True)
         elif method == "muscle":
-            cline = MuscleCommandline(muscle_exe, parameters)
-        stdout, stderr = cline()
+            cline = MuscleCommandline(input=in_dir)
+        stdout, _ = cline()
+
+        # read the command line output string
         align = AlignIO.read(StringIO(stdout), "fasta")
     return align
 
